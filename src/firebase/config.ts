234a610a5bdaa1ed987c050b25718adec354e310
RamaChandra Murthy Mamidipalli <ramachandramurthymamidipalli@gmail.com>
2026-05-03 03:04:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-542336256-e1199",
+  "appId": "1:244589976698:web:fca38146210f7853ac2fc3",
+  "apiKey": "AIzaSyAEoTvvjDKYmBKxX0e6FS84vsnim4Gec1I",
+  "authDomain": "studio-542336256-e1199.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "244589976698"
+};
